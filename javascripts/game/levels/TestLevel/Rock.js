9beba155493f91b729f0014a94e0efd703970b4f
Ben Myers <bmyers@appnexus.com>
2014-12-03 01:04:47 -0500

    Forgot to commit Rock.js, oops

---
+++ javascripts/game/levels/TestLevel/Rock.js
@@ -0,0 +1,20 @@
+define([
+	'game/classes/actor/MovingFullCollisionActor',
+	'create!game/display/Sprite > Mailman'
+], function(
+	SUPERCLASS,
+	MailmanSprite
+) {
+	function Rock(x, y) {
+		SUPERCLASS.call(this, x, y, MailmanSprite.width, MailmanSprite.height);
+	}
+	Rock.prototype = Object.create(SUPERCLASS.prototype);
+	Rock.prototype.recalculateCollisionBoxes = function() {
+		this.collidableBox = this.boundingBox;
+	};
+	Rock.prototype.render = function(ctx, camera) {
+		MailmanSprite.render(ctx, camera, this.x, this.y, 1, false);
+		SUPERCLASS.prototype.render.call(this, ctx, camera);
+	};
+	return Rock;
+});
